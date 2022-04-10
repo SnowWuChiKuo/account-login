7e6310f400e86dbf572fe9e02e7b92d54356d925
SnowWuChiKuo <x9x9x8x8x7x7x6x6x@gmail.com>
2022-04-10 20:27:19 +0800

    add tips

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const exphbs = require('express-handlebars')
-const Account = require('./models/account')
+const express = require('express')         // 載入 express
+const mongoose = require('mongoose')       // 載入 mongoose
+const exphbs = require('express-handlebars')  // 載入 handlebars
+const Account = require('./models/account')   // 載入 models裡的account
 
-const app = express()
+const app = express()    
 const port = 3000
 
 mongoose.connect('mongodb://localhost/account-login', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -29,7 +29,7 @@ app.post('/', (req, res) => {
   Account
     .findOne(postUser)
     .lean()
-    .then(data => data ? data : res.redirect('/'))
+    .then(data => data ? data : res.redirect('/')) // 確認是否與資料庫的資料相同
     .then(data => res.render('show', { data }))
     .catch(error => console.log(error))
 })
